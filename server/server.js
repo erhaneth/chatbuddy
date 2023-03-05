441471784fbf979406fc62ce94240f29acde6019
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-03-05 00:44:00 -0800

    debugging

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
-const configureation = new Configuration({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+const openai = new OpenAIApi(configuration);
 const app = express();
 app.use(cors());
 app.use(express.json());
